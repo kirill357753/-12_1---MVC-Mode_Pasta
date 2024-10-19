a10aad0b327993ee66ac1e884081a53f64f48095
Козлов Кирилл <aleksmyskinov088@gmail.com>
2024-10-20 00:25:10 +0300

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -10,22 +10,26 @@ if __name__ == "__main__":
        ingredients = ["спагетти", "бекон", "пармезан", "чёрный перец", "соль", "оливковое масло", "яичные желтки"],
        price = 450 ,
        weight = 600,
-       picture =  image_path
+       picture =  image_path,
+       additional_ingredients = ["+макароны", "+бекон", "+пармезана"]
     )
     my_controller = pasta_controllers(my_model)
     my_view = pasta_view(my_controller)
     my_view.show_cafe_menu()
     my_view.show_site_menu()
+    my_view.save_order_to_json("Паста карбонара_+")
+    #my_view.get_data_from_json('Admin', r'orders\1827407360.54_карбонара_Стандарт')
+    print()
     my_view.set_ingredients('user', ["спагетти", "бекон", "пармезан", "оливковое масло", "яичные желтки"])
-    my_view.set_ingredients('admin', ("спагетти", "бекон", "пармезан", "оливковое масло", "яичные желтки"))
-    my_view.set_ingredients('admin', ["спагетти", "бекон", "пармезан", "оливковое масло", "яичные желтки"])
+    my_view.set_ingredients('Admin', ("спагетти", "бекон", "пармезан", "оливковое масло", "яичные желтки"))
+    my_view.set_ingredients('Admin', ["спагетти", "бекон", "пармезан", "оливковое масло", "яичные желтки"])
     print()
     my_view.set_price('user', "550")
-    my_view.set_price('admin', "пятьсот пятьдесят")
-    my_view.set_price('admin', "550")
+    my_view.set_price('Admin', "пятьсот пятьдесят")
+    my_view.set_price('Admin', "550")
+    print()
+    my_view.set_weight('user', "550")
+    my_view.set_weight('Admin', "пятьсот пятьдесят")
+    my_view.set_weight('Admin', "550")
     print()
     my_view.show_cafe_menu()
-
-
-
-
